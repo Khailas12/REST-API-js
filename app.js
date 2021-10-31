3dbb6756d2a0b2c889d7d12aa9550d8bed04f25e
Khailas R <khailas303@gmail.com>
2021-11-01 04:14:03 +0530

    CRUD

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 const http = require('http');
-const hostname = 'localhost'
+const todos = require('./data');
+const todos = require('./data');
+const hostname = 'localhost';
 const PORT = process.env.PORT || 5000;
+const { getReqData } = require('./utils');
 
 
-const server = http.createServer(async (req, res) => {
-    // to set the request route/url
-    if (req.url === '/api' && req.method === 'GET') {
+const server = http.createServer(async (request, res) => {
+    //  /api/todos : GET
+    if (request.url === '/api/todos' && request.method === 'GET') {
+        // read
+        const todos = await new todos().getTodos();
         res.writeHead(200, {
             'Content-type': 'application/json'
-        });  //response headers
-
-        res.write('Hello World');   // set response
-        res.end();
-    } 
-    
-    // if there's no route
-    else {    
-        res.writeHead(400, {
-            'Content-type': 'application/json'
         });
-        res.end(JSON.stringify({ message: 'Route unavailable'}));
+        res.end(JSON.stringify(todos));     // send data
     }
+
+    else if (request.url.match(/\/api\/([0-9]+)/) && request.method==='GET') {
+        try {
+            const id = request.url.split('/')[3];               // get id from url 
+            const todo = await new Todo().getTodos(id);
+            request.writeHead(200, {
+                'Content-type': 'application/json'
+            });
+        }
+        catch(error) {
+            request.writeHead(404, {
+                'Content-type': 'application/json'
+            });
+            request.end(JSON.stringify({ message: error }));
+        }
+    }
+
+    // delete
+    else if (request.url.match(/\/api\/todos\/([0-9]+)/) && request.method==='DELETE');
+    try {9
+        const id = request.url.split('/')[3];
+        let message = await new Todo().deleteTodo(id);
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        req.end(JSON.stringify({ message}));    // send message
+    } 
+    catch (error) {
+        res.writeHead(404, {
+            'Content-Type': 'application/json'
+        });     // send status code and content-type
+
+        res.end(JSON.stringify({ message: error }));    // send the error
+    }
+    
 });
+
+
 
 
 // this indicates when the server is up and running
